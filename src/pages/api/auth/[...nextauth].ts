905e6bd6a18faaca47f478189abaf76f0a1c9bf8
ervandenbosch <ezravdb@gmail.com>
2023-11-29 23:49:12 +0100

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -10,18 +10,18 @@ export default NextAuth({
                 password: { label: 'Password', type: 'password', placeholder: 'password' },
             },
             authorize: async (credentials) => {
-                if (!credentials || !credentials.username || !credentials.password) {
-                    throw new Error('/signin?error=empty-credentials');
-                }
-
-                if (
-                    credentials.username === process.env.NEXT_AUTH_USERNAME &&
-                    credentials.password === process.env.NEXT_AUTH_PASSWORD
-                ) {
-                    return Promise.resolve({ accepted: true });
-                } else {
-                    throw new Error('/signin?error=invalid-credentials');
-                }
+                return new Promise((resolve, reject) => {
+                    if (!credentials || !credentials.username || !credentials.password) {
+                        reject('/signin?error=empty-credentials');
+                    } else if (
+                        credentials.username === process.env.NEXT_AUTH_USERNAME &&
+                        credentials.password === process.env.NEXT_AUTH_PASSWORD
+                    ) {
+                        resolve({ accepted: true });
+                    } else {
+                        reject('/signin?error=invalid-credentials');
+                    }
+                });
             },
         }),
     ],
