c006ab344354ba2517ca9f8b8a12ff01f9b83563
ervandenbosch <ezravdb@gmail.com>
2023-12-26 13:48:21 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,24 +12,26 @@ export default function Home() {
 
     return (
         <>
-            <div
-                className="bg local mt-[48px] flex h-[88vh] w-full flex-row bg-cover bg-no-repeat sm:flex-col"
-                id="top"
-                style={{
-                    backgroundImage: 'url("/images/cover.png")',
-                }}
-            >
-                <div className=" ml-32 flex w-full flex-col justify-end text-white lg:mb-12 lg:items-start md:justify-center sm:m-auto sm:mb-6 sm:items-center sm:justify-center">
+            <div className="mt-[48px] flex h-[88vh] w-full flex-row sm:flex-col" id="top">
+                <img src="/images/cover.png" className="absolute h-[86vh] w-full object-cover" />
+                <div className="relative ml-32 flex w-full flex-col justify-end text-white lg:mb-12 lg:items-start md:justify-center sm:m-auto sm:mb-6 sm:items-center sm:justify-center">
                     <div className="font-aileron text-[25px]"> MONIKA & EZRA</div>
                     <div className="font-notoreg text-[80px] italic md:text-[60px]">our</div>
-                    <div className="mb-8 whitespace-nowrap font-notoreg text-[180px] lg:text-[120px] md:text-[80px]">
+                    <div className="mb-12 whitespace-nowrap font-notoreg text-[180px] lg:text-[120px] md:text-[80px]">
                         Big Day
                     </div>
                 </div>
-                <div className="mr-18 mb-24 w-full self-end text-center font-serif text-4xl tracking-wide text-white md:self-center sm:mb-14 sm:text-2xl">
+                <div className="mr-18 relative mb-32 w-full self-end text-center font-serif text-4xl tracking-wide text-white md:self-center sm:mb-14 sm:text-2xl">
                     Saturday,
                     <div>27 July 2024,</div>
                     <div>France</div>
+                    <div className="mt-12 pr-4 text-white">
+                        <Link href="https://54deicxmihb.typeform.com/to/ULCDxC4s">
+                            <button className=" py-0.2 ml-8  rounded-xl border-2 border-gray-400 bg-white px-12 text-gray-800 md:px-6">
+                                RSVP
+                            </button>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </>
